7c6df6e597219aedb949abcfbb4c5e20c6b75d56
myname-jin <adsd336@daum.net>
2024-11-21 20:42:03 +0900

    ㅇㅇ
    ㅇㅇㅇ

--- js/jscode.js
+++
@@ -1,7 +0,0 @@
-function goToPage(target) {
-    if (target === 'men') {
-      window.location.href = 'placeholder-men.html';
-    } else if (target === 'women') {
-      window.location.href = 'placeholder-women.html';
-    }
-  }
