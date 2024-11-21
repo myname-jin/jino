f3ab236a12cd17cf45099917b88377a2e776f36e
myname-jin <adsd336@daum.net>
2024-11-22 00:00:12 +0900

    달라진건 없음

--- js/jscode.js
+++ js/jscode.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // "MEN" 버튼 클릭 시 naver.com으로 이동
   document.getElementById('men').addEventListener('click', function () {
     window.location.href = 'https://naver.com'; // 현재 창에서 naver.com으로 이동
   });
 
+  // "WOMEN" 버튼 클릭 시 Mainpage.html로 이동
   document.getElementById('women').addEventListener('click', function () {
-    window.location.href = 'https://daum.net'; // 현재 창에서 daum.net으로 이동
+    window.location.href = 'Mainpage.html'; // 현재 창에서 Mainpage.html로 이동
   });
 });
