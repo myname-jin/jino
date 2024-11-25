a67500cbf6787a6746e0afe3987ae844bb7c25c9
myname-jin <adsd336@daum.net>
2024-11-25 14:38:24 +0900

    Update main.js

--- html/main.js
+++ html/main.js
@@ -3,5 +3,5 @@ function goToMen() {
 }
 
 function goToWomen() {
-  window.location.href = "../html/woman_main/woman_main.html"; // 이동하고자 하는 URL
+  window.location.href = "../html/women_main/women_main.html"; // 이동하고자 하는 URL
 }
